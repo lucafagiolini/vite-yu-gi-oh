2f69eb815495002a5687be84e4ebe56b1dbc8b17
Luca <lucafagiolini@live.it>
2024-03-05 16:21:00 +0100

    api is working correctly

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
-<script >
+<script>
+import axios from 'axios';
+import {store} from './store.js';
+
+export default {
+ data() {
+   return {
+
+     store,
+   }
+  },
+
+   created() {
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then(response => {
+      console.log(response.data.data); 
+      this.store.cards = response.data.data;
+    });
+    
+   },
+ 
+
+}
 
 </script>
 
 <template>
+  hello
   
 </template>
 
-<style >
+<style lang="scss" >
 
 </style>
